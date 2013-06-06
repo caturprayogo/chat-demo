803e165c3a4de009e9f959228f9b261b1f0c1e8e
crisboot <cortez.cristian@gmail.com>
2013-06-06 17:35:35 -0300

    Adding passport initialize and session

--- app.js
+++ app.js
@@ -44,6 +44,8 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
